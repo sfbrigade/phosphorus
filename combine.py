9a008c605e1778e8bd27ca041946ce05feaf2ff7
Mick Thompson <dthompson@gmail.com>
2013-03-27 18:38:19 -0700

    so in progres

---
+++ combine.py
@@ -0,0 +1,24 @@
+import csv
+
+count = -1
+
+with open('combined.csv', 'wb') as csvoutputfile:
+    csvwriter = csv.writer(csvoutputfile, delimiter=',',
+                            quotechar='"', quoting=csv.QUOTE_ALL)
+
+    with open('sfbusinesses.csv', 'rb') as csvfile:
+        sfreader = csv.reader(csvfile, delimiter=',', quotechar='"')
+
+
+        # "business_id","name","address","city","state","postal_code","latitude","longitude","phone_number"
+        for r in sfreader:
+            csvwriter.writerow([count]+r+[""])
+            count+=1
+
+    #0,id,name,url,phone,address,city,postal_code,latitude,longitude
+    with open('yelpbusinesses.csv', 'rb') as csvfile2:
+        yreader = csv.reader(csvfile2, delimiter=',', quotechar='"')
+
+        for r in yreader:
+            csvwriter.writerow([count,"",r[2],r[5],r[6],r[7],"CA",r[8],r[9],r[4],r[3]])
+            count+=1
