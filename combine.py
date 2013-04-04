047dd67a2c078cb4658a606211bc3e00c07f15bf
Mick Thompson <dthompson@gmail.com>
2013-04-03 21:02:54 -0700

    get rid of businesses with no name

--- combine.py
+++ combine.py
@@ -12,13 +12,17 @@ with open('combined.csv', 'wb') as csvoutputfile:
 
         # "business_id","name","address","city","state","postal_code","latitude","longitude","phone_number"
         for r in sfreader:
-            csvwriter.writerow([count]+r+[""])
-            count+=1
+            if(r[1] != ""):
+                csvwriter.writerow([count]+r+[""])
+                count+=1
 
     #0,id,name,url,phone,address,city,postal_code,latitude,longitude
     with open('yelpbusinesses.csv', 'rb') as csvfile2:
         yreader = csv.reader(csvfile2, delimiter=',', quotechar='"')
-
+        first = False
         for r in yreader:
+
             csvwriter.writerow([count,"",r[2],r[5],r[6],r[7],"CA",r[8],r[9],r[4],r[3]])
-            count+=1
+            if(first):
+                count+=1
+            first = True
